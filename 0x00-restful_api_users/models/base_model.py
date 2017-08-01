adf3da5c0a1f018f0fe386eb451892e396417f65
kris <krisbredemeier@gmail.com>
2017-08-01 09:40:22 -0700

    compiels, but does not assign correct valsue to var

--- 0x00-restful_api_users/models/base_model.py
+++ 0x00-restful_api_users/models/base_model.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 from datetime import datetime
+from uuid import *
 
 class BaseModel():
     id = None
     created_at = None
     updated_at = None
     def __init__(self):
-        self._id = str(UUID)
+        self._id = uuid4()
         self._created_at = datetime.utcnow()
         self._updated_at = datetime.utcnow()
