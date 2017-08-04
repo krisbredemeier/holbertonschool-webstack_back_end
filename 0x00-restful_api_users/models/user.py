e014d8d11f5abdc5da8d2ccf6cf50c1adcd44927
kris <krisbredemeier@gmail.com>
2017-08-03 18:48:18 -0700

    initates user

--- 0x00-restful_api_users/models/user.py
+++ 0x00-restful_api_users/models/user.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
-from BaseModel import BaseModel
+from models.base_model import BaseModel
+
+'''
+Write a class User that defines the user model:
+
+Inherits from BaseModel
+Public class attributes:
+email initialized to None
+first_name initialized to None
+last_name initialized to None
+Protected class attributes:
+_password initialized to None
+'''
 
 class User(BaseModel):
 
+    email = None
+    first_name = None
+    last_name = None
+    __password = None
+
     def __init__(self):
-        self.email = None
-        self.first_name = None
-        self.last_name = None
-        self.__password = None
+        self.email = 
+        self.first_name =
+        self.last_name =
+        self.__password =
