b4530ae35f5872b80a2c70b8c692fe77cf8ff3ca
kris <krisbredemeier@gmail.com>
2017-08-03 18:35:15 -0700

    craate user

--- 0x00-restful_api_users/models/user.py
+++ 0x00-restful_api_users/models/user.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+from BaseModel import BaseModel
+
+class User(BaseModel):
+
+    def __init__(self):
+        self.email = None
+        self.first_name = None
+        self.last_name = None
+        self.__password = None
