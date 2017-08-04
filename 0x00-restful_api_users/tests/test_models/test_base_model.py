551f16b6fdf3779766fbedf579f97956fdf7e9e2
kris <krisbredemeier@gmail.com>
2017-08-03 18:27:26 -0700

    creates working tests

---
+++ 0x00-restful_api_users/tests/test_models/test_base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+'''
+Does id, created_at and updated_at have a value when
+you create a new instance of the BaseModel class?
+Is id is unique?
+etc...
+'''
+
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+class TestBaseModel(unittest.TestCase):
+
+    def setUp(self):
+        self.my_base_model = BaseModel()
+
+    def test_id(self):
+        # check if id is unique
+        bm = BaseModel()
+        self.assertNotEqual(bm.id, self.my_base_model.id)
+        self.assertTrue(type(self.my_base_model.id) == str)
+
+    def test_created_at(self):
+        # test if created_at not empty
+        self.assertNotEqual(self.my_base_model.created_at, None)
+        self.assertTrue(self.my_base_model.created_at, datetime.utcnow())
+
+    def test_updated_at(self):
+        # test if updated_at is not empty
+        self.assertNotEqual(self.my_base_model.updated_at, None)
+        self.assertTrue(self.my_base_model.updated_at, datetime.utcnow())
+
+if __name__ == '___main__':
+    unittest.main()
