2f1feb43a0418211f8fa2e3de59ddda3fe3da559
kris <krisbredemeier@gmail.com>
2017-08-08 12:39:40 -0700

    checks for invalid password

--- 0x00-restful_api_users/models/user.py
+++ 0x00-restful_api_users/models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
+import hashlib
 
 '''
 Write a class User that defines the user model:
@@ -30,14 +31,30 @@ class User(BaseModel):
         if self.first_name == None:
             return self.last_name
         else:
-            return '{0} {1}'.format(self.first_name, self.last_name)
+            return '{} {}'.format(self.first_name, self.last_name)
 
     def __str__(self):
-        return '{0} {1} - {2} - {3}'.format('[User]', self.id, self.email, self.display_name())
+        return '{} {} - {} - {}'.format('[User]', self.id, self.email, self.display_name())
 
-    # @setter
-    # def get_password(self):
-    #     return self.__password
-    #
-    # @getter
-    # def set_password(self, password):
+    # creates getter to encrypt user password
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        if value == None:
+            self._password = None
+        else:
+            self._password = hashlib.md5(value.encode("utf8")).hexdigest()
+
+    # validates that the value passed is the clear
+    # version of the password of a User instance
+    def is_valid_password(self, pwd):
+        if pwd == None or pwd =! str or self._password == None:
+            return False
+        pwd = hashlib.md5(value.encode("utf8")).hexdigest()
+        if pwd == self._password:
+            return True
+        else:
+            return False
