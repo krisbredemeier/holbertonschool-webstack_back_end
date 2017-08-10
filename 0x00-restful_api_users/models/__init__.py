fd09119f14d8f4e0d873d6e7fd9e75fbd4d3751c
kris <krisbredemeier@gmail.com>
2017-08-09 19:03:20 -0700

    formatting

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 from sqlalchemy import create_engine
-from sqlalchemy import drop_all
-from sqlalchemy import created_at
-from sqlalchemy import scope_session
-from sqlalchemy import sessionmaker
+from sqlalchemy.orm import drop_all
+from sqlalchemy.orm import created_at
+from sqlalchemy.orm import scope_session
+from sqlalchemy.orm import sessionmaker
 import os
 
 db_engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(os.environ.get('HBNB_YELP_MYSQL_USER'), os.environ.get('HBNB_YELP_MYSQL_PWD'), os.environ.get('HBNB_YELP_MYSQL_HOST'), os.environ.get('HBNB_YELP_MYSQL_DB')))
