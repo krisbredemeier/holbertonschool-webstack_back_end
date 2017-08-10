87359bbbc3c1b7d18e17ae7ed019063e5d3471e8
kris <krisbredemeier@gmail.com>
2017-08-09 19:45:28 -0700

    formatting

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
+'''
+Create an object called db_engine, an instance of a SQLAlchemy Engine object,
+by using create_engine:
+you must use the mysql+mysqldb engine
+MySQL host, user, password and database
+must be defined as environment variables:
+host: HBNB_YELP_MYSQL_HOST
+user: HBNB_YELP_MYSQL_USER
+password: HBNB_YELP_MYSQL_PWD
+database: HBNB_YELP_MYSQL_DB
+If HBNB_YELP_ENV=test, drop all tables by using drop_all
+Create all tables by using create_all
+Create an object called db_session, an instance of SQLAlchemy Session,
+by using scoped_session and sessionmaker:
+bind option must be set to the db_engine object
+expire_on_commit option must be set to False
+'''
+
 from sqlalchemy import create_engine
-# from sqlalchemy import drop_all
 from sqlalchemy import schema
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
