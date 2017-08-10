d40fd1cb3c6bb263d20178f72221d118a0d15998
kris <krisbredemeier@gmail.com>
2017-08-09 18:41:39 -0700

    formats to fit python

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 from sqlalchemy import create_engine, case
-db_engine = create_engine("mysql[+mysqldb]://HBNB_YELP_MYSQL_USER:HBNB_YELP_MYSQL_PWD@HBNB_YELP_MYSQL_HOST/HBNB_YELP_MYSQL_DB")
-case([
-        (HBNB_YELP_ENV=='test'),
-        (drop_all(bind=None, tables=None, checkfirst=True))
-    ])
+db_engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
+    env['HBNB_YELP_MYSQL_USER'],
+    env['HBNB_YELP_MYSQL_PWD'],
+    env['HBNB_YELP_MYSQL_HOST'],
+    env['HBNB_YELP_MYSQL_DB']
+    )
+
+if env['HBNB_YELP_ENV']=='test':
+    drop_all(bind=None, tables=None, checkfirst=True)
+
 create_all(bind=None, tables=None, checkfirst=True)
-db_session = sessionmaker(bind=db_engine, expire_on_commit=False)
+db_session = scope_session(sessionmaker(bind=db_engine, expire_on_commit=False))
