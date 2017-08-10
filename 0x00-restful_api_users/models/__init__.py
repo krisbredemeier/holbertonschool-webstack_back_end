953e7ab2fe7daae06fc6103794b0cbcab28ec034
kris <krisbredemeier@gmail.com>
2017-08-09 18:20:01 -0700

    where statement for sql

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 from sqlalchemy import create_engine
 db_engine = create_engine("mysql[+mysqldb]://HBNB_YELP_MYSQL_USER:HBNB_YELP_MYSQL_PWD@HBNB_YELP_MYSQL_HOST/HBNB_YELP_MYSQL_DB")
-if HBNB_YELP_ENV=test:
-    drop_all(bind=None, tables=None, checkfirst=True)
+    where (
+        case(
+            {HBNB_YELP_ENV=test},
+            drop_all(bind=None, tables=None, checkfirst=True)
+            )
+        )
 create_all(bind=None, tables=None, checkfirst=True)
 
 db_session = sessionmaker(bind=db_engine, expire_on_commit=False)
