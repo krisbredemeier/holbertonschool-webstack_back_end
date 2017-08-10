0858dce702849ce69dfb560c714807f56465d735
kris <krisbredemeier@gmail.com>
2017-08-09 18:17:05 -0700

    changes init"

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from sqlalchemy import create_engine
-db_engine = create_engine(mysql+mysqldb://HBNB_YELP_MYSQL_USER:HBNB_YELP_MYSQL_PWD@HBNB_YELP_MYSQL_HOST/HBNB_YELP_MYSQL_DB)
+db_engine = create_engine("mysql[+mysqldb]://HBNB_YELP_MYSQL_USER:HBNB_YELP_MYSQL_PWD@HBNB_YELP_MYSQL_HOST/HBNB_YELP_MYSQL_DB")
 if HBNB_YELP_ENV=test:
     drop_all(bind=None, tables=None, checkfirst=True)
 create_all(bind=None, tables=None, checkfirst=True)
