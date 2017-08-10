f6794c3b2b1de7fd6895e69e0f8a165a3b314398
kris <krisbredemeier@gmail.com>
2017-08-10 08:49:22 -0700

    adds base

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -26,10 +26,10 @@ import os
 metadata = schema.MetaData()
 
 db_engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(os.environ.get('HBNB_YELP_MYSQL_USER'), os.environ.get('HBNB_YELP_MYSQL_PWD'), os.environ.get('HBNB_YELP_MYSQL_HOST'), os.environ.get('HBNB_YELP_MYSQL_DB')))
-metadata.bind = db_engine
+Base.metadata.bind = db_engine
 
 if os.environ.get('HBNB_YELP_ENV') == 'test':
     drop_all()
 
-metadata.create_all()
+Base.metadata.create_all()
 db_session = scoped_session(sessionmaker(bind=db_engine, expire_on_commit=False))
