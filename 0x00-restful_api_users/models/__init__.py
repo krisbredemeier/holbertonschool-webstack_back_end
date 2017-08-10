c2e1a632b7c1aebee5b757dadf575b2c3dff721e
kris <krisbredemeier@gmail.com>
2017-08-09 19:01:18 -0700

    formatting

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 from sqlalchemy import create_engine
-from sqlalchemy import MetaData
-from sqlalchemy import Table
-from sqlalchemy import Column
-from sqlalchemy import Integer, String
+from sqlalchemy import drop_all
+from sqlalchemy import created_at
+from sqlalchemy import scope_session
+from sqlalchemy import sessionmaker
 import os
 
 db_engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(os.environ.get('HBNB_YELP_MYSQL_USER'), os.environ.get('HBNB_YELP_MYSQL_PWD'), os.environ.get('HBNB_YELP_MYSQL_HOST'), os.environ.get('HBNB_YELP_MYSQL_DB')))
 
-metadata = MetaData(db_engine)
 
 if os.environ.get('HBNB_YELP_ENV') == 'test':
-    metadata.drop_all(bind=None, tables=None, checkfirst=True)
+    drop_all(bind=None, tables=None, checkfirst=True)
 
-metadata.create_all(bind=None, tables=None, checkfirst=True)
+create_all(bind=None, tables=None, checkfirst=True)
 db_session = scope_session(sessionmaker(bind=db_engine, expire_on_commit=False))
