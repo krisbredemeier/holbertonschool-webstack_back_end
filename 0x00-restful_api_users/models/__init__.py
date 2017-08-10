f15992405ab3301854547ac5968143fcd7040ae9
kris <krisbredemeier@gmail.com>
2017-08-09 18:22:01 -0700

    removes indent

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from sqlalchemy import create_engine
 db_engine = create_engine("mysql[+mysqldb]://HBNB_YELP_MYSQL_USER:HBNB_YELP_MYSQL_PWD@HBNB_YELP_MYSQL_HOST/HBNB_YELP_MYSQL_DB")
-    case(
-        {HBNB_YELP_ENV=test},
-        drop_all(bind=None, tables=None, checkfirst=True)
-        )
+case(
+    {HBNB_YELP_ENV=test},
+    drop_all(bind=None, tables=None, checkfirst=True)
+    )
 create_all(bind=None, tables=None, checkfirst=True)
 
 db_session = sessionmaker(bind=db_engine, expire_on_commit=False)
