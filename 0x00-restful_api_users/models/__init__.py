a2f4193822c0673c3b90305659358bb2b9fa5283
kris <krisbredemeier@gmail.com>
2017-08-09 18:44:22 -0700

    formatting

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine, case
 db_engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(env['HBNB_YELP_MYSQL_USER'], env['HBNB_YELP_MYSQL_PWD'], env['HBNB_YELP_MYSQL_HOST'], env['HBNB_YELP_MYSQL_DB'])
 
-if (env['HBNB_YELP_ENV'] == 'test'):
+if env['HBNB_YELP_ENV'] == 'test':
     drop_all(bind=None, tables=None, checkfirst=True)
 
 create_all(bind=None, tables=None, checkfirst=True)
