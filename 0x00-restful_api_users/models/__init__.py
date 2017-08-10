1b645afe0a9662dedea605a24e37bb5c039dde6e
kris <krisbredemeier@gmail.com>
2017-08-09 18:58:04 -0700

    formatting

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -8,7 +8,7 @@ import os
 
 db_engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(os.environ.get('HBNB_YELP_MYSQL_USER'), os.environ.get('HBNB_YELP_MYSQL_PWD'), os.environ.get('HBNB_YELP_MYSQL_HOST'), os.environ.get('HBNB_YELP_MYSQL_DB')))
 
-metadata = MetaData(engine)
+metadata = MetaData(db_engine)
 
 if os.environ.get('HBNB_YELP_ENV') == 'test':
     metadata.drop_all(bind=None, tables=None, checkfirst=True)
