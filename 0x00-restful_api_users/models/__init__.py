a16077a8ecf9f5670e1eabdfee7253afc9a9a1b8
kris <krisbredemeier@gmail.com>
2017-08-09 19:20:32 -0700

    formatting

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 # from sqlalchemy import created_at
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
-from flask_sqlalchemy import SQLAlchemy
 import os
 
 db_engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(os.environ.get('HBNB_YELP_MYSQL_USER'), os.environ.get('HBNB_YELP_MYSQL_PWD'), os.environ.get('HBNB_YELP_MYSQL_HOST'), os.environ.get('HBNB_YELP_MYSQL_DB')))
