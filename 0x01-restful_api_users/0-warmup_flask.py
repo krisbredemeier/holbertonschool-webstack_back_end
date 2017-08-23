9d1334c8ce0ca37437d110093ac946c77a665320
kris <krisbredemeier@gmail.com>
2017-08-22 18:47:38 -0700

    strict slashes

--- 0x01-restful_api_users/0-warmup_flask.py
+++ 0x01-restful_api_users/0-warmup_flask.py
@@ -2,17 +2,25 @@
 
 from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
 def run_flask():
     '''
     start flask application
     '''
-    app = Flask(__name__)
-    # app.run(host='34.206.234.184', port=35129)
-    # app.url_map.strict_slashes = False
-    # print("Holberton")
-    app.run(host='0.0.0.0', port=5000, strict_slashes=False)
     app.url_map.strict_slashes = False
-    print("Holberton")
+    return "Holberton School"
 
 if __name__ == "__main__":
-    run_flask()
+    app.run(host='0.0.0.0', port=5000)
+
+
+# app = Flask(__name__)
+#
+# @app.route('/', methods=['GET'])
+# def test():
+#     print("Holberton")
+#
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=5000)
