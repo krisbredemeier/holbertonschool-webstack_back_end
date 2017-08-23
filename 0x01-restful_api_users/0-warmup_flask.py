f2104a8c154a00daad6e1f47f8266522c982b86d
kris <krisbredemeier@gmail.com>
2017-08-22 18:22:32 -0700

    inital psudo code, but cannot connect to server

---
+++ 0x01-restful_api_users/0-warmup_flask.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+def run_flask():
+    '''
+    start flask application
+    '''
+    app = Flask(__name__)
+    app.run(host='34.206.234.184', port=35129)
+    app.url_map.strict_slashes = False
+    print("Holberton")
+    # app.run(host='0.0.0.0', port=5000, strict_slashes=False)
+    # app.url_map.strict_slashes = False
+if __name__ == "__main__":
+    run_flask()
