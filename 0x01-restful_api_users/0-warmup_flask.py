863140abb525ba4799c66cab9445a14018a90582
kris <krisbredemeier@gmail.com>
2017-08-22 18:28:09 -0700

    prepares for container

--- 0x01-restful_api_users/0-warmup_flask.py
+++ 0x01-restful_api_users/0-warmup_flask.py
@@ -7,10 +7,12 @@ def run_flask():
     start flask application
     '''
     app = Flask(__name__)
-    app.run(host='34.206.234.184', port=35129)
+    # app.run(host='34.206.234.184', port=35129)
+    # app.url_map.strict_slashes = False
+    # print("Holberton")
+    app.run(host='0.0.0.0', port=5000, strict_slashes=False)
     app.url_map.strict_slashes = False
     print("Holberton")
-    # app.run(host='0.0.0.0', port=5000, strict_slashes=False)
-    # app.url_map.strict_slashes = False
+
 if __name__ == "__main__":
     run_flask()
