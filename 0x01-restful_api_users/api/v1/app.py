2f7cef2830bc966460fafd74617539922751f29d
kris <krisbredemeier@gmail.com>
2017-08-31 16:08:25 -0700

    works on psudo code for task 5

--- 0x01-restful_api_users/api/v1/app.py
+++ 0x01-restful_api_users/api/v1/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 from flask import Flask
 from api.v1.views import app_views
+app = Flask(__name__)
 
 app_views.register_blueprint(url_prefix='/api/v1')
 
 if __name__ == "__main__":
-    app_views.run(host=HBNB_API_HOST, port=HBNB_API_PORT)
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT)
