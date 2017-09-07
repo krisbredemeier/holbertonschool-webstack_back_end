ef60ce8000d98b0350a6aa2a51290d8b548d66fd
kris <krisbredemeier@gmail.com>
2017-09-07 16:51:27 -0700

    index fits pep8

--- 0x01-restful_api_users/api/v1/views/index.py
+++ 0x01-restful_api_users/api/v1/views/index.py
@@ -2,8 +2,7 @@
 from flask import Flask
 from api.v1.views import app_views
 
-app_views.strict_slashes = False
 
-@app_views.route('/status', methods=['GET'], strict_slashes = False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def test():
     return '{"status" : "OK"}'
