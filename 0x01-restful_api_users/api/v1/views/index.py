d47eef8afbff6dfd08176bde0625161c4d79f9d8
kris <krisbredemeier@gmail.com>
2017-09-07 16:52:56 -0700

    cleans up files and comments

--- 0x01-restful_api_users/api/v1/views/index.py
+++ 0x01-restful_api_users/api/v1/views/index.py
@@ -5,4 +5,7 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def test():
+    '''
+    runs quick test on status
+    '''
     return '{"status" : "OK"}'
