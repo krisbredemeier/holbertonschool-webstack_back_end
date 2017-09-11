2f36409b3c8150d90345b7e12601648c55afde54
kris <krisbredemeier@gmail.com>
2017-09-10 17:05:02 -0700

    adds comment to method

--- 0x01-restful_api_users/3-warmup_flask.py
+++ 0x01-restful_api_users/3-warmup_flask.py
@@ -24,21 +24,11 @@ HBNB_API_PORT = os.environ.get('HBNB_API_PORT')
 HBNB_API_HOST = os.environ.get('HBNB_API_HOST')
 
 
-@app.route('/', methods=['GET'])
-def get_holberton():
-    '''
-    start flask application
-    '''
-    return "Holberton School"
-
-
-@app.route('/c', methods=['GET'])
-def get_C():
-    return "C is fun!"
-
-
 @app.route('/hbtn', methods=['GET'])
 def get_hbtn_data():
+    '''
+    get hbtn route
+    '''
     return jsonify(C="is fun",Python="is cool", Sysadmin="is hiring")
 
 
