ad63818ce5012094db620b4b55100f489d44833b
kris <krisbredemeier@gmail.com>
2017-09-28 17:38:07 -0700

    auth class

---
+++ 0x02-restful_api_users/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+'''
+Now we will create a class
+to manage the API authentication.
+'''
+
+from flask import request
+
+class Auth():
+
+
+    def require_auth(self, path, excluded_paths):
+        '''
+        hat returns False -
+        path and excluded_paths will be used later, now,
+        you don't need to take care of them
+        '''
+        return False
+
+
+    def authorization_header(self, request=None):
+        '''
+        that returns None -
+        request will be the Flask request object
+        '''
+        return None
+
+
+    def current_user(self, request=None):
+        '''
+        that returns None -
+        request will be the Flask request object
+        '''
+        return None
