e232ed897cf6d82cf889523ca41842f05e351eff
kris <krisbredemeier@gmail.com>
2017-09-29 17:37:16 -0700

    works on auth

--- 0x02-restful_api_users/api/v1/auth/auth.py
+++ 0x02-restful_api_users/api/v1/auth/auth.py
@@ -16,14 +16,20 @@ class Auth():
         path and excluded_paths will be used later, now,
         you don't need to take care of them
         '''
-        return False
+        if path is None or excluded_paths is None or excluded_paths is Null:
+            return True
+        if path in excluded_paths:
+            return False
 
     def authorization_header(self, request=None):
         '''
         that returns None -
         request will be the Flask request object
         '''
-        return None
+        if resquest is None or Authorization not in request:
+            return None
+        else:
+            return(Authorization)
 
     def current_user(self, request=None):
         '''
