ec6cbafb0754d43270d2fa8ab7871814918d3576
kris <krisbredemeier@gmail.com>
2017-10-01 17:10:01 -0700

    base64

--- 0x02-restful_api_users/api/v1/auth/BasicAuth.py
+++ 0x02-restful_api_users/api/v1/auth/BasicAuth.py
@@ -6,6 +6,20 @@ to manage the API authentication.
 '''
 
 from flask import request
-
+from api.v1.auth.auth import Auth
 
 class BasicAuth():
+
+    def extract_base64_authorization_header(self, authorization_header):
+        '''
+         returns the Base64 part of the Authorization header
+         for a Basic Authentication:
+        '''
+        if auth.authorization_header is None:
+            return None
+        if not str(auth.authorization_header):
+            return None
+        if Basic not in auth.authorization_header:
+            return None
+        else:
+            return Basic
