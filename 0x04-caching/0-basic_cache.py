89581a56cb354e7fcb246a40aaa34906ba852b8c
kris <krisbredemeier@gmail.com>
2018-01-17 10:41:37 -0800

    partially works

--- 0x04-caching/0-basic_cache.py
+++ 0x04-caching/0-basic_cache.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
 from base_caching import BaseCaching
 import datetime
-import caches
-
+# import requests
+# import requests_cache
+#
+# requests_cache.install_cache('cache_data')
 
 class BasicCache(BaseCaching):
     '''
     class that inherits from BaseCaching
     '''
 
-    def put(self, key, item):
-        self.cache_data[key] = {'value': item}
-        if self.cache_data[key] is None or item is None
-            return
-    def get(self, key):
-        if self.cashe_data[key] is None or key not in self.cache_data:
-            return None
-
     def __init__(self):
         '''Constructor'''
-        self.cashe_data = {}
+        self.cache_data = {}
+
+
+    def put(self, key, item):
+        ''' assign to dictionary'''
+        self.cache_data[key] = {'value': item}
+        if self.cache_data[key] is None or item is None:
+            return
+
+
+    def get(self, key):
+        '''return value linked to key '''
+        if self.cashe_data[key] is None or key not in self.cache_data:
+            return None
