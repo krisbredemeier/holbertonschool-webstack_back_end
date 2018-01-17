f65281b0125cb73f179f6f46aa390b49667f6cce
kris <krisbredemeier@gmail.com>
2018-01-17 14:06:34 -0800

    getting closer

--- 0x04-caching/1-fifo_cache.py
+++ 0x04-caching/1-fifo_cache.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 from base_caching import BaseCaching
-import datetime
 
 class FIFOCache(BaseCaching):
     '''class that inherits from BaseCaching '''
@@ -9,14 +8,17 @@ class FIFOCache(BaseCaching):
         '''Constructor'''
         self.cache_data = {}
         self.MAX_ITEMS = len(self.cache_data)
+        super().__init__()
 
     def put(self, key, item):
         '''set max_items for caching'''
         current_len = len(self.cache_data)
+        if key is None or item is None:
+            pass
+        if not key or not item:
+            pass
         self.cache_data[key] = item
-        if self.cache_data[key] is None or item is None:
-            return
-        if key not in self.cache_data and self.MAX_ITEMS >= current_len:
+        if self.MAX_ITEMS < current_len:
             self.remove_first()
 
     def get(self, key):
@@ -25,18 +27,13 @@ class FIFOCache(BaseCaching):
             return None
         if key is None:
             return None
+        return self.cache_data[key]
 
     def remove_first(self):
         '''remove the last added entry'''
-        first_entry = None
-        for key in self.cache_data:
-            if first_entry is None:
-                first_entry = key
-            elif self.cache_data[key] < self.cache_data[first_entry][
-            'first_entry']:
-                first_entry = key
-            self.cache_data.pop(first_entry)
-            print("DISCARD:{key}")
+        first_entry = list(self.cache_data)[0];
+        self.cache_data.pop(first_entry)
+        print("DISCARD: {}".format(first_entry))
 
     @property
     def size(self):
