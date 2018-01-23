0d7ec560fa6b4296c3da556712e5957b55fdc21c
kris <krisbredemeier@gmail.com>
2018-01-23 15:48:46 -0800

    reoganizes code to get correct output

--- 0x04-caching/0-basic_cache.py
+++ 0x04-caching/0-basic_cache.py
@@ -13,7 +13,7 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         ''' assign to dictionary'''
-        if self.cache_data[key] is None or item is None:
+        if key is None or item is None:
             pass
         self.cache_data[key] = item
 
