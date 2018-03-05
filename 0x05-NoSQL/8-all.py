e3cffb3ff2c9471b87d4850cbbe6614ce27db340
kris <krisbredemeier@gmail.com>
2018-03-04 16:09:59 -0800

    puts code into main file

--- 0x05-NoSQL/8-all.py
+++ 0x05-NoSQL/8-all.py
@@ -1,10 +1,4 @@
 from pymongo import MongoClient
 
-def list_all(mongo_collection)
-if __name__ == '__main__':
-    client = MongoClient("localhost", 27017, maxPoolSize=50)
-    db = client.localhost
-    collection = db['school']
-    cursor = collection.find({})
-    for document in cursor:
-          print(document)
+def list_all(mongo_collection):
+    
