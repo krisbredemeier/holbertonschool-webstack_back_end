98098e41a1fb1be5e8c24d80ceb02bdf79cfd69e
kris <krisbredemeier@gmail.com>
2018-03-04 16:20:49 -0800

    pymongo

--- 0x05-NoSQL/8-all.py
+++ 0x05-NoSQL/8-all.py
@@ -1,4 +1,12 @@
-from pymongo import MongoClient
+#!/usr/bin/python3
+# from pymongo import MongoClient
+
+conMongo = MongoClient(connect, 27017)
+listOfDBs = conMongo.database_names()
 
 def list_all(mongo_collection):
-    
+    for in in listOfDBs:
+        db = conMongo[i]
+        print db
+        pring db.colection_names():
+    conMongo.disconnect()
