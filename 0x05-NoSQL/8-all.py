d7a7138f09a7ff8941e98f53702aa91d13289876
kris <krisbredemeier@gmail.com>
2018-03-04 16:41:16 -0800

    mongo py

--- 0x05-NoSQL/8-all.py
+++ 0x05-NoSQL/8-all.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from pymongo import MongoClient
+Client = MongoClient('mongodb://127.0.0.1:27017')
+db = Client.admin
 
 def list_all(mongo_collection):
     lst_all = db.school.find()
-    retrun (lst_all)
+    return (lst_all)
