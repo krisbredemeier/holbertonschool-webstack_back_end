9f3686495aa3e248859dc69f73074c652f105100
kris <krisbredemeier@gmail.com>
2018-03-05 18:50:21 -0800

    returns empty list

--- 0x05-NoSQL/8-all.py
+++ 0x05-NoSQL/8-all.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
+''' Write a Python function that lists all documents in a collection '''
 from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
-    # db = client.school
-    # documents = db.school.find()
-    # for document in documents:
-    #     print (document)
-    total_count = mongo_collection.count
+    ''' count collection to see if it's empty '''
+    total_count = mongo_collection.count()
     if total_count == 0:
         print("this collection is empty")
+        return []
     else:
         return mongo_collection.find()
---
+++ 0x05-NoSQL/9-insert_school.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+from pymongo import MongoClient
+
+def insert_school(mongo_collection, **kwargs):
