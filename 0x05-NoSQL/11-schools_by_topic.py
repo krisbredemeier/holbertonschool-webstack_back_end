bcbab3f2589039d35d893cbefe48729634056284
kris <krisbredemeier@gmail.com>
2018-03-06 08:45:08 -0800

    check if collection is empty for testing

--- 0x05-NoSQL/11-schools_by_topic.py
+++ 0x05-NoSQL/11-schools_by_topic.py
@@ -6,4 +6,9 @@ from pymongo import MongoClient
 
 def schools_by_topic(mongo_collection, topic):
     ''' find and return school based on topic '''
-    return mongo_collection.find({ "topic": topic })
+    total_count = mongo_collection.count({ "topic": topic })
+    if total_count == 0:
+        print("this collection is empty")
+        return []
+    else :
+        return mongo_collection.find({ "topic": topic })
