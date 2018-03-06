fc4424b19fe823dcb8d76be94bec3bb39f92cfb9
kris <krisbredemeier@gmail.com>
2018-03-06 08:57:37 -0800

    if name main outline

--- 0x05-NoSQL/12-log_stats.py
+++ 0x05-NoSQL/12-log_stats.py
@@ -4,3 +4,15 @@ Write a Python script that provides some
 stats about Nginx logs stored in MongoDB
 '''
 from pymongo import MongoClient
+
+
+
+
+
+''' estiablish db connection and MongoClient '''
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    school_collection = client.my_db.school
+    print("{} logs".format(logs))
+    for method in methods:
+        print("Methods: /n/t method {}: {} ".format(method))
