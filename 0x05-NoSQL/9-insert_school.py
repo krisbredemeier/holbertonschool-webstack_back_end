f91501e3304e2f7d5599ac57f1b27ddffbba9e45
kris <krisbredemeier@gmail.com>
2018-03-05 18:56:20 -0800

    return new id

--- 0x05-NoSQL/9-insert_school.py
+++ 0x05-NoSQL/9-insert_school.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+'''
+Write a Python function that inserts a
+new document in a collection based on kwargs
+'''
 from pymongo import MongoClient
 
 def insert_school(mongo_collection, **kwargs):
+    ''' return new id'''
+    mongo_collection.insert(kwargs)
+    return mongo_collection.new_school_id
