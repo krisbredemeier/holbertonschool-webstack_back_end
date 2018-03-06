864e77b28c516aebb23de7db441ca9a0448b7ef3
kris <krisbredemeier@gmail.com>
2018-03-06 08:42:41 -0800

    find school based on topic

--- 0x05-NoSQL/11-schools_by_topic.py
+++ 0x05-NoSQL/11-schools_by_topic.py
@@ -3,3 +3,7 @@
 Write a Python function that returns the list of school having a specific topic
 '''
 from pymongo import MongoClient
+
+def schools_by_topic(mongo_collection, topic):
+    ''' find and return school based on topic '''
+    return mongo_collection.find({ "topic": topic })
