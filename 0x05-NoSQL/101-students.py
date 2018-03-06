d8967658ad2ac57c9009b4a1ee9b6013b67ea27a
kris <krisbredemeier@gmail.com>
2018-03-06 10:26:28 -0800

    not quite sorting properly yet

--- 0x05-NoSQL/101-students.py
+++ 0x05-NoSQL/101-students.py
@@ -3,7 +3,8 @@
 Write a Python function that returns all students sorted by average score
 '''
 from pymongo import MongoClient
+import pymongo
 
 def top_students(mongo_collection):
     ''' sort students by score '''
-    db.mongo_collection.sort({})
+    return mongo_collection.find().sort({ "score": pymongo.ASCENDING })
