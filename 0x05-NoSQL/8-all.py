5067b3858332cb267465ad7938e23dd6c5574533
kris <krisbredemeier@gmail.com>
2018-03-05 17:59:54 -0800

    iterates though document

--- 0x05-NoSQL/8-all.py
+++ 0x05-NoSQL/8-all.py
@@ -5,5 +5,6 @@ client = MongoClient('mongodb://127.0.0.1:27017')
 
 def list_all(mongo_collection):
     db = client.school
-    lst_all = db.school.find({"name": "Holberton school"})
-    print (lst_all)
+    documents = db.school.find()
+    for document in documents:
+        print (document)
