e84d8683fe7b7beefd50287e000d6065a5209d14
kris <krisbredemeier@gmail.com>
2018-03-09 15:07:15 -0800

    removes test print message

--- 0x05-NoSQL/11-schools_by_topic.py
+++ 0x05-NoSQL/11-schools_by_topic.py
@@ -8,7 +8,7 @@ def schools_by_topic(mongo_collection, topic):
     ''' find and return school based on topic '''
     total_count = mongo_collection.count({ "topic": topic })
     if total_count == 0:
-        print("this collection is empty")
+        # print("this collection is empty")
         return []
     else :
         return mongo_collection.find({ "topic": topic })
