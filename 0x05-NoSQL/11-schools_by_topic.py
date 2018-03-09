db287ded4530f9a758fa176d7e621fffd8386277
kris <krisbredemeier@gmail.com>
2018-03-09 15:20:44 -0800

    pep8 formatting for 11

--- 0x05-NoSQL/11-schools_by_topic.py
+++ 0x05-NoSQL/11-schools_by_topic.py
@@ -4,11 +4,12 @@ Write a Python function that returns the list of school having a specific topic
 '''
 from pymongo import MongoClient
 
+
 def schools_by_topic(mongo_collection, topic):
     ''' find and return school based on topic '''
-    total_count = mongo_collection.count({ "topic": topic })
+    total_count = mongo_collection.count({"topic": topic})
     if total_count == 0:
         # print("this collection is empty")
         return []
-    else :
-        return mongo_collection.find({ "topic": topic })
+    else:
+        return mongo_collection.find({"topic": topic})
