4ffbaadecc0d6bc1ec09580969792165b50948c4
kris <krisbredemeier@gmail.com>
2018-03-11 20:45:11 -0700

    trouble shooting by taking out if else statement looking for empty collection

--- 0x05-NoSQL/11-schools_by_topic.py
+++ 0x05-NoSQL/11-schools_by_topic.py
@@ -7,9 +7,9 @@ from pymongo import MongoClient
 
 def schools_by_topic(mongo_collection, topic):
     ''' find and return school based on topic '''
-    total_count = mongo_collection.count({"topic": topic})
-    if total_count == 0:
-        # print("this collection is empty")
-        return []
-    else:
-        return mongo_collection.find({"topics": {"$eq": topic}})
+    # total_count = mongo_collection.count({"topic": topic})
+    # if total_count == 0:
+    #     # print("this collection is empty")
+    #     return []
+    # else:
+    return mongo_collection.find({"topics": {"$eq": topic}})
