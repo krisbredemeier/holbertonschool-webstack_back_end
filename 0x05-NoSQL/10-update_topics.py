8f68be8d01f6b5b9a3cdc1314d3125eb3fb28594
kris <krisbredemeier@gmail.com>
2018-03-11 20:37:59 -0700

    use multi permater since the output is not correct

--- 0x05-NoSQL/10-update_topics.py
+++ 0x05-NoSQL/10-update_topics.py
@@ -9,4 +9,4 @@ from pymongo import MongoClient
 def update_topics(mongo_collection, name, topics):
     ''' name of school to update with update_topics'''
     mongo_collection.update({"name": name},
-                            {"$set": {"topics": topics}})
+                            {"$set": {"topics": topics}}, multi=True)
